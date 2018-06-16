5171ed7f6831484c6c25fbcb2b90ec5fe141016e
Paisaclub <infopaisaclub@gmail.com>
2018-06-16 20:23:02 +0530

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
+var browserSync = require('browser-sync').create();
+
+
+// Compile SCSS
+gulp.task('css:compile', function() {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass.sync({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+});
+
+// Minify CSS
+gulp.task('css:minify', ['css:compile'], function() {
+  return gulp.src([
+      './css/*.css',
+      '!./css/*.min.css'
+    ])
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+
+// CSS
+gulp.task('css', ['css:compile', 'css:minify']);
+
+// Minify JavaScript
+gulp.task('js:minify', function() {
+  return gulp.src([
+      './js/*.js',
+      '!./js/*.min.js'
+    ])
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./js'))
+    .pipe(browserSync.stream());
+});
+
+// JS
+gulp.task('js', ['js:minify']);
+
+// Default task
+gulp.task('default', ['css', 'js']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+// Dev task
+gulp.task('dev', ['css', 'js', 'browserSync'], function() {
+  gulp.watch('./scss/*.scss', ['css']);
+  gulp.watch('./js/*.js', ['js']);
+  gulp.watch('./*.html', browserSync.reload);
+});
---
+++ js/app.js
@@ -0,0 +1,30 @@
+instances = null;
+document.addEventListener('DOMContentLoaded', function() {
+    var elem_sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elem_sidenav,{edge:'right'});
+
+    var elem_scroll = document.querySelectorAll('.scrollspy');
+    M.ScrollSpy.init(elem_scroll);
+
+    var elem_carousel = document.querySelectorAll('.carousel');
+    instances = M.Carousel.init(elem_carousel,{fullWidth: true, indicators:true});   
+    var elem_floating_button = document.querySelectorAll('.fixed-action-btn');
+    M.FloatingActionButton.init(elem_floating_button);
+    var elem_tap = document.querySelectorAll('.tap-target');
+    M.TapTarget.init(elem_tap);
+    var elem_modal = document.querySelectorAll('.modal');
+    M.Modal.init(elem_modal);
+    var ell = document.querySelectorAll('.portfolio-content');
+    ell.forEach(function(element){
+      element.style.height = (element.clientWidth)+'px';
+    });
+    
+    autoplay();
+  });
+
+  function autoplay(){
+    instances.forEach(function(element) {
+      element.next();
+    }, this);
+    setTimeout(autoplay,4000);
+  }
